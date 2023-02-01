3155338cf4b320be3c2744c890f733ffc36fbf3c
Anna Oliinyk <anna.oleinik666@gmail.com>
2023-02-01 22:00:15 +0100

    Fixed

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,20 +1,18 @@
 import User from '../User';
 import s from './style.module.css'
 import { useState } from "react";
+import { useEffect } from 'react';
 
 function App() {
-  let defArr = [];
-  fetch(`https://jsonplaceholder.typicode.com/users`)
-    .then(response => response.json())
-    .then((items) =>{
-      items.forEach(element => {
-        defArr.push(element)
-      });
-    } )
-    .catch(() => {
-      console.log("error")
-    })
-    const [users, setUsers] = useState(defArr);
+  useEffect(() => {
+    (async () => {
+      const resp = await fetch (`https://jsonplaceholder.typicode.com/users`);
+      const data = await resp.json();
+      setUsers(data);
+    })()
+  },[])
+  const [users, setUsers] = useState([]);
+
   const deleteUser = (delId) => {
     const newArr = users.filter(({id}) => id !== delId);
     setUsers(newArr);
